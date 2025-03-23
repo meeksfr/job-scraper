d116e278020271ec192e6114e89b8d24cd617810
meeksfr <mika.sustar@mail.utoronto.ca>
2025-03-22 21:01:49 -0400

    java spring boot backend init

---
+++ jobScraper/src/main/java/ca/mikaonline/jobScraper/JobScraperApplication.java
@@ -0,0 +1,13 @@
+package ca.mikaonline.jobScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobScraperApplication.class, args);
+	}
+
+}
